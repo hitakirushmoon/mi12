e155b0f350c0f546a36beadebaf30e3cce84b803
hitakirushmoon <minhmarty@gmail.com>
2021-09-02 19:11:43 +0700

    webhook

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+     express = require('express'),
+     bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'))
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+    res.status(200).send('EVENT_RECEIVED')
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
